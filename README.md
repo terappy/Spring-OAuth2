eed92644a427a7437a04516c74d702e50b236e82
terappy <terakei818@gmail.com>
2018-03-06 00:19:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Security OAuth2 Test
+
+Qiitaで解説記事書いてます。
+[Spring SecurityのOAuth2を使ってLogin,Logoutをしよう]()
+
+# Author
+[@terappy](https://github.com/terappy)
---
+++ src/main/java/com/example/Security/OAuth2/test/SecurityOAuth2TestApplication.java
@@ -0,0 +1,12 @@
+package com.example.Security.OAuth2.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOAuth2TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityOAuth2TestApplication.class, args);
+	}
+}
