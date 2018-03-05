dee4bf9901013f3173df91abc59f5b9105a72bba
terappy <terakei818@gmail.com>
2018-03-06 03:54:27 +0900

    細かな修正

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Spring SecurityでOAuth2を使ったLogin,Logoutを行うだけのもの
 
 Qiitaで解説記事を書きました。
 
-[Spring SecurityのOAuth2を使ってLogin,Logoutをしよう]()
+[Spring SecurityのOAuth2を使ってLogin,Logoutをしよう](https://qiita.com/terappy/items/5ea297f5e7b8e334308e)
 
 # Author
 
--- src/main/java/com/example/Security/OAuth2/test/controller/MainController.java
+++ src/main/java/com/example/Security/OAuth2/test/controller/MainController.java
@@ -1,10 +1,6 @@
 package com.example.Security.OAuth2.test.controller;
 
-import java.security.Principal;
-
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
